a6eda632c2db68147f9dfcb9ab5ce8593849d4f6
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-09-03 22:12:52 +0630

    user collabive issue fixed

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,16 +1,14 @@
 'use server';
-import { nanoid } from "nanoid";
-import { liveblocks } from "../liveblocks";
-import { revalidatePath } from 'next/cache';
-import { parseStringify } from "../utils";
 
-interface CreateDocumentParams {
-  userId: string;
-  email: string;
-}
+import { nanoid } from 'nanoid'
+import { revalidatePath } from 'next/cache';
+import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
+import { liveblocks } from '../liveblocks';
 
 export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
   const roomId = nanoid();
+
   try {
     const metadata = {
       creatorId: userId,
@@ -27,25 +25,27 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
       usersAccesses,
       defaultAccesses: []
     });
+    
     revalidatePath('/');
+
     return parseStringify(room);
   } catch (error) {
-    console.error(`Error happened while creating a room: ${error}`);
-    throw new Error('Failed to create document');
+    console.log(`Error happened while creating a room: ${error}`);
   }
-};
+}
 
 export const getDocument = async ({ roomId, userId }: { roomId: string; userId: string }) => {
   try {
-    const room = await liveblocks.getRoom(roomId);
-    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
-    if (!hasAccess) {
-      throw new Error('You do not have access to this document');
-    }
-
-    return parseStringify(room);
+      const room = await liveblocks.getRoom(roomId);
+    
+      const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    
+      if(!hasAccess) {
+        throw new Error('You do not have access to this document');
+      }
+    
+      return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while getting a room: ${error}`);
-    throw new Error('Failed to retrieve document');
   }
-};
+}
